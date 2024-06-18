4e49cad532d5f397bd9df68e30514dd306eef143
Ceclik <yura11122345@gmail.com>
2024-06-18 23:30:25 +0300

    Fire aim mode a little rebalanced

--- Assets/Scripts/FireAimScripts/FireSplitter.cs
+++ Assets/Scripts/FireAimScripts/FireSplitter.cs
@@ -30,14 +30,14 @@ namespace FireAimScripts
         
         private float CountFireStopSpeed()  //this counting must be remade in universal 
         {
-            if (_timer is >= 8.0f and < 9.0f) return 0.7f;
-            if (_timer is >= 9.0f and < 10.0f) return 0.65f;
-            if (_timer is >= 10.0f and < 11.0f) return 0.55f;
-            if (_timer is >= 11.0f and < 12.0f) return 0.45f;
-            if (_timer is >= 12.0f and < 13.0f) return 0.4f;
-            if (_timer is >= 13.0f and < 14.0f) return 0.35f;
-            if (_timer >= 14.0f) return 0.3f;
-            return 0.3f;
+            if (_timer is >= 8.0f and < 9.0f) return 0.55f;
+            if (_timer is >= 9.0f and < 10.0f) return 0.5f;
+            if (_timer is >= 10.0f and < 11.0f) return 0.4f;
+            if (_timer is >= 11.0f and < 12.0f) return 0.3f;
+            if (_timer is >= 12.0f and < 13.0f) return 0.25f;
+            if (_timer is >= 13.0f and < 14.0f) return 0.2f;
+            if (_timer >= 14.0f) return 0.2f;
+            return 0.2f;
         }
 
         private void Update()
