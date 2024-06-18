88112c0e073ba10ce67be87a671f5ce8f0b91172
Ceclik <yura11122345@gmail.com>
2024-06-18 19:02:02 +0300

    Timer size increasing when low added

--- Assets/Scripts/FireAimScripts/FireSplitter.cs
+++ Assets/Scripts/FireAimScripts/FireSplitter.cs
@@ -11,6 +11,9 @@ namespace FireAimScripts
         [SerializeField] private TextMeshProUGUI timerText;
         
         private float _timer;
+        private bool _isTimerIncreased;
+        private Vector3 _startLocalScale;
+        private Vector3 _endLocalScale;
         
         public delegate void DecreaseLives();
         public event DecreaseLives OnFireSplitted;
@@ -18,12 +21,20 @@ namespace FireAimScripts
         private void Start()
         {
             _timer = Random.Range(minTimeToSplit, maxTimeToSplit + 1.0f);
+            _startLocalScale = new Vector3(1.0f, 1.0f, 1.0f);
+            _endLocalScale = new Vector3(3.0f, 3.0f, 3.0f);
         }
 
         private void Update()
         {
             _timer -= Time.deltaTime;
             timerText.text = ((int)_timer).ToString();
+
+            if (_timer < 3.0f && !_isTimerIncreased)
+            {
+                timerText.rectTransform.localScale = Vector3.Lerp(_startLocalScale, _endLocalScale, 2.0f);
+                _isTimerIncreased = true;
+            }
 
             if (_timer <= 0)
             {
@@ -34,6 +45,8 @@ namespace FireAimScripts
 
         private void SplitFire()
         {
+            timerText.rectTransform.localScale = _startLocalScale;
+            _isTimerIncreased = false;
             OnFireSplitted?.Invoke();
         }
     }
